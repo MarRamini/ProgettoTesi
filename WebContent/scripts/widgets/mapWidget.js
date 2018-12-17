a97bbea7e2e81e749c18669fa48f7af2918f7836
MarRamini <mar.ramini@stud.uniroma3.it>
2018-12-17 12:36:58 +0100

    3d

--- WebContent/scripts/widgets/mapWidget.js
+++ WebContent/scripts/widgets/mapWidget.js
@@ -24,6 +24,24 @@ require([
 	    id: "mapView"
 	  });
 	  
+      sceneLayer = new SceneLayer({
+          portalItem: {
+              //id: "327d6b60509f4ddab12ee5a424fac7de"
+              id: "2342ab7928834076a1240fb93c60e978"
+          },
+          elevationInfo: {
+            //mode: "absolute-height",
+            //offset: 6
+          }
+      });
+      
+      /*sceneLayer = new SceneLayer({
+          url: "http://scene.arcgis.com/arcgis/rest/services/Hosted/Building_Berlin1/SceneServer",
+          layerId: 0
+      });*/
+      
+      map.add(sceneLayer);
+	  
 	  view.ui.components.forEach(function(component){
 		  if(component != "attribution"){
 			  view.ui.move(component, "top-right");
